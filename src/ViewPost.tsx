a31c7db6fe84470eb79f85b08c1e7c12f8061b54
Sean Nguyen <seannguyen2919@gmail.com>
2020-05-21 23:09:52 -0400

    view post

---
+++ src/ViewPost.tsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import { API, graphqlOperation } from 'aws-amplify'
+import { getPost } from './graphql/queries'
+import { RouteComponentProps } from '@reach/router';
+
+interface ViewPostProps extends RouteComponentProps {
+  pid?: string;
+}
+const ViewPost : React.FC<ViewPostProps> = ({ pid }) => {
+  const [post, setPost] = useState({title:'', description:''})
+  useEffect(() => {
+    fetchPost()
+  }, [])
+
+  async function fetchPost() {
+    try {
+      console.log(pid);
+      const postData : any = await API.graphql(graphqlOperation(getPost, {id: pid}))
+      console.log(postData)
+      setPost(postData.data.getPost)
+    } catch (err) { console.log('error fetching post') }
+  }
+
+  return(
+    <React.Fragment>
+      <input
+        type="text"
+        placeholder={post.title}
+      />
+      <input
+        type="text"
+        placeholder={post.description}
+      />
+    </React.Fragment>
+  )
+}
+
+export default ViewPost;
