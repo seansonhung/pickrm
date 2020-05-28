ced519d0ad7a0d8e3e3d7518daa7c8de9a508dce
Sean Nguyen <seannguyen2919@gmail.com>
2020-05-27 22:40:06 -0400

    add header layout

---
+++ src/layout/Header.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+type HeaderProps = {
+};
+
+const Header: React.FC<HeaderProps> = () => {
+
+  return (
+    <NotSignedInOptions />
+  )
+};
+
+const NotSignedInOptions = () => (
+  <React.Fragment>
+  </React.Fragment>
+);
+
+export default Header;
