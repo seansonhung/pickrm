83798826dee19a7b3d9943fc72263a038e321a45
Sean Nguyen <seannguyen2919@gmail.com>
2020-06-04 06:05:45 -0400

    add components for entries

---
+++ src/components/ExpiredEntry.tsx
@@ -0,0 +1,16 @@
+import React, { useState} from 'react';
+import { API, graphqlOperation } from 'aws-amplify'
+
+type EntrySubmittedProps = {
+  pid?: string;
+}
+const EntrySubmitted : React.FC<EntrySubmittedProps> = ({ pid }) => {
+
+
+  return(
+    <React.Fragment>
+    </React.Fragment>
+  )
+}
+
+export default EntrySubmittedProps;
