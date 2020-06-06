3a0034ee466d2abc18ae8a2b0917973221a5f595
Sean Nguyen <seannguyen2919@gmail.com>
2020-06-06 05:06:51 -0400

    added header with menu. Add link to items

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,18 +1,147 @@
-import React, { useState } from "react";
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InfoIcon from '@material-ui/icons/Info';
+import LibraryAddIcon from '@material-ui/icons/LibraryAdd';
+import LibraryBooksIcon from '@material-ui/icons/LibraryBooks';
+import MailIcon from '@material-ui/icons/Mail';
+import { Link } from '@reach/router';
 
-type HeaderProps = {
-};
+const Header: React.FC = () => {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
 
-const Header: React.FC<HeaderProps> = () => {
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
 
   return (
-    <NotSignedInOptions />
-  )
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar
+        position="static"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, open && classes.hide)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            Pickrm
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="left"
+        open={open}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          <ListItem button component={Link} to="/app/create" key="create box" >
+            <ListItemIcon> <LibraryAddIcon /> </ListItemIcon>
+            <ListItemText primary="Create Pickrm Box"/>
+          </ListItem>
+          <ListItem button key="view boxes">
+            <ListItemIcon> <LibraryBooksIcon /> </ListItemIcon>
+            <ListItemText primary="Your Pickrm Boxes"/>
+          </ListItem>
+        </List>
+        <Divider />
+        <List>
+          <ListItem button key="about">
+            <ListItemIcon> <InfoIcon /> </ListItemIcon>
+            <ListItemText primary="About Pickrm"/>
+          </ListItem>
+          <ListItem button key="Contact/Suggestions">
+            <ListItemIcon> <MailIcon /> </ListItemIcon>
+            <ListItemText primary="Contact/Suggestions"/>
+          </ListItem>
+        </List>
+      </Drawer>
+    </div>
+  );
 };
 
-const NotSignedInOptions = () => (
-  <React.Fragment>
-  </React.Fragment>
-);
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginBottom: theme.spacing(6)
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  },
+}));
+
 
 export default Header;
--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import Footer from "./Footer";
+import Header from "./Header";
+import { Container } from '@material-ui/core';
+import "../style/global.css";
 
 type LayoutProps = {
   children: React.ReactNode;
@@ -9,8 +12,11 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
 
   return (
     <React.Fragment>
+      <Header />
+      <Container component="div" maxWidth="lg">
       <main>{children}</main>
       <Footer />
+      </Container>
     </React.Fragment>
   );
 };
