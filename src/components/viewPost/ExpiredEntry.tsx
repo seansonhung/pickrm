f083b21e40c635fe206dc173f758535ea2943582
Sean Nguyen <seannguyen2919@gmail.com>
2020-07-09 05:42:07 -0400

    expired entry text

--- src/components/viewPost/ExpiredEntry.tsx
+++ src/components/viewPost/ExpiredEntry.tsx
@@ -1,16 +1,12 @@
 import React, { useState} from 'react';
-import { API, graphqlOperation } from 'aws-amplify'
 
-type EntrySubmittedProps = {
-  pid?: string;
-}
-const EntrySubmitted : React.FC<EntrySubmittedProps> = ({ pid }) => {
-
+const ExpiredEntry : React.FC = () => {
 
   return(
     <React.Fragment>
+      <h1> The time to reply has ended. This post no longer accepting any new entry.</h1>
     </React.Fragment>
   )
 }
 
-export default EntrySubmittedProps;
+export default ExpiredEntry;
