445ceb621b3144e9430e6da0ac74b996afb7fda0
Sean Nguyen <seannguyen2919@gmail.com>
2020-07-09 05:41:43 -0400

    need to fix data not updating as database updates

--- src/Pages/ViewPost.tsx
+++ src/Pages/ViewPost.tsx
@@ -4,23 +4,34 @@ import { getPost } from '../graphql/queries'
 import { RouteComponentProps } from '@reach/router';
 import CreateEntry from '../components/viewPost/CreateEntry';
 import PostDetails from '../components/viewPost/PostDetails';
+import ExpiredEntry from '../components/viewPost/ExpiredEntry';
 import { Grid, Paper } from '@material-ui/core';
+import CountDown from '../components/viewPost/Countdown';
 
 interface ViewPostProps extends RouteComponentProps {
   pid?: string;
 }
 const ViewPost : React.FC<ViewPostProps> = ({ pid }) => {
-  const [post, setPost] = useState({title:'', description:'', expiredDate:''})
+  const [post, setPost] = useState({title:'', description:'', expiredDate:'', winningEntrie:''})
+  const [postEntries, setPostEntries] =  useState(Array());
+  const [postExpired, setPostExpired] = useState(false);
+
+  let seconds = (new Date (post.expiredDate).valueOf() - Date.now()) / 1000;
+
   useEffect(() => {
     fetchPost()
   }, [])
 
   async function fetchPost() {
     try {
-      console.log(pid);
       const postData : any = await API.graphql(graphqlOperation(getPost, {id: pid}))
+      if (postData.data.getPost.winningEntrie != null){
+        setPostExpired(true);
+      }
+      setPost(postData.data.getPost);
+
+      setPostEntries(postEntries => [...postEntries, postData.data.getPost.entries.items]);
       console.log(postData)
-      setPost(postData.data.getPost)
     } catch (err) { console.log('error fetching post') }
   }
 
@@ -33,17 +44,32 @@ const ViewPost : React.FC<ViewPostProps> = ({ pid }) => {
             <PostDetails
               postTitle={post.title}
               postDescription={post.description}
-              postExpiredDate={new Date (post.expiredDate)}
+              winningEntry = {post.winningEntrie}
             />
+            {!isNaN(seconds)? 
+            <CountDown
+              pid={pid}
+              seconds={seconds}
+              winningEntry={post.winningEntrie}
+              entries = {postEntries}
+            />
+            : ""}
           </Paper>
         </Grid>
         {/* Post Entries Actions */}
         <Grid item xs={12} md={5} lg={5}>
+        {postExpired 
+        ?
+          <Paper>
+            <ExpiredEntry/>
+          </Paper>
+        :
           <Paper>
             <CreateEntry 
               pid={pid}
             />
           </Paper>
+        }
         </Grid>
       </Grid>
     </React.Fragment>
